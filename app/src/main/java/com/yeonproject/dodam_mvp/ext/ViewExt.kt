d2df59a8a0988f83de088a59faa76841a256d7c1
LEEYEONJAE <eunoiaa22@gmail.com>
2020-04-20 18:26:12 +0900

    Remove unused function

--- app/src/main/java/com/yeonproject/dodam_mvp/ext/ViewExt.kt
+++ app/src/main/java/com/yeonproject/dodam_mvp/ext/ViewExt.kt
@@ -5,19 +5,11 @@ import android.net.Uri
 import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
-import java.io.File
 
 fun ImageView.glideImageSet(image: String, width: Int, height: Int) {
     Glide.with(context)
         .load(image)
         .override(width, height)
-        .centerCrop()
-        .into(this)
-}
-
-fun ImageView.glideImageSet(image: String) {
-    Glide.with(context)
-        .load(image)
         .centerCrop()
         .into(this)
 }
@@ -30,20 +22,7 @@ fun ImageView.glideImageSet(image: Uri, width: Int, height: Int) {
         .into(this)
 }
 
-fun ImageView.glideImageSet(image: Int, width: Int, height: Int) {
-    Glide.with(context)
-        .load(image)
-        .override(width, height)
-        .centerCrop()
-        .into(this)
-}
-
 fun Context.shortToast(resourceId: Int) {
     Toast.makeText(this, resourceId, Toast.LENGTH_SHORT)
-        .show()
-}
-
-fun Context.shortToast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT)
         .show()
 }
