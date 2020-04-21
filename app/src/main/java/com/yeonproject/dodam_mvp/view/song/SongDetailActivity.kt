d084456bb9d701da22a6398d3c19362198fbc9b5
LEEYEONJAE <eunoiaa22@gmail.com>
2020-04-21 19:13:23 +0900

    Add song web view

--- app/src/main/java/com/yeonproject/dodam_mvp/view/song/SongDetailActivity.kt
+++ app/src/main/java/com/yeonproject/dodam_mvp/view/song/SongDetailActivity.kt
@@ -1,5 +1,6 @@
 package com.yeonproject.dodam_mvp.view.song
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.yeonproject.dodam_mvp.R
@@ -14,6 +15,7 @@ class SongDetailActivity : AppCompatActivity() {
 
         tv_name.text = name
         tv_link.text = "www.youtube.com/watch?v=$id"
+
         val youTubePlayerView = you_tube_player_view
         if (id != null) {
             youTubePlayerView.play(id)
@@ -22,10 +24,17 @@ class SongDetailActivity : AppCompatActivity() {
         btn_back.setOnClickListener {
             finish()
         }
+
+        tv_link.setOnClickListener {
+            val intent = Intent(this, SongWebViewActivity::class.java)
+            intent.putExtra(LINK, tv_link.text.toString())
+            startActivity(intent)
+        }
     }
 
     companion object {
         private const val ID = "id"
         private const val NAME = "name"
+        private const val LINK = "link"
     }
 }
---
+++ app/src/main/java/com/yeonproject/dodam_mvp/view/song/SongWebViewActivity.kt
@@ -0,0 +1,34 @@
+package com.yeonproject.dodam_mvp.view.song
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import androidx.appcompat.app.AppCompatActivity
+import com.yeonproject.dodam_mvp.R
+import kotlinx.android.synthetic.main.activity_song_web_view.*
+
+class SongWebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_web_view)
+        val uri = intent.getStringExtra(LINK)
+        setWebView("https://$uri")
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    fun setWebView(uri: String) {
+        web_view.run {
+            webChromeClient = WebChromeClient()
+            settings.setSupportMultipleWindows(true)
+            settings.javaScriptEnabled = true
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.useWideViewPort = true
+            settings.builtInZoomControls = true
+            loadUrl(uri)
+        }
+    }
+
+    companion object {
+        private const val LINK = "link"
+    }
+}
